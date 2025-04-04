4ee74d83c92b03e26190fe9cd7a0f3b6c6c63c57
ibrohim0725 <baxtiyorovibrohim513@gmail.com>
2025-04-04 10:54:35 +0500

    add winner user gradient

--- js/ui-changer.js
+++ js/ui-changer.js
@@ -2,32 +2,34 @@ import { findWinner } from "./find-winner.js";
 import { elRobot, elScore, elStatus, elUser } from "./html-elements.js";
 import { robotChooser } from "./robot-chooser.js";
 
-export function uiChanger(variant){
-    elUser.src =`./img/${variant}.svg`;
+export function uiChanger(variant) {
+  elUser.src = `./img/${variant}.svg`;
 
-    setTimeout(()=>{
-        const robot=robotChooser()
-        elRobot.src=`./img/${robot}.svg`;
+  setTimeout(() => {
+    const robot = robotChooser();
+    elRobot.src = `./img/${robot}.svg`;
 
-        const winner=findWinner(variant ,robot);
-        let count=parseInt(sessionStorage.getItem('count'))||0;
-     
-        if(winner==='tied'){
-            elStatus.innerText="TIED"
-        }else if(winner==='user'){
-            elStatus.innerText="YOU WIN"
-             count++  }
-        else{
-            elStatus.innerText="YOU LOSE"
-            if(count >= 1){
-                count--;
-            }else{
-                count
-            }
-           
-        }
-        elScore.innerText=count;
-        sessionStorage.setItem("count" ,count)
+    const winner = findWinner(variant, robot);
+    let count = parseInt(sessionStorage.getItem("count")) || 0;
 
-    },1000)
+    if (winner === "tied") {
+      elStatus.innerText = "TIED";
+    } else if (winner === "user") {
+      elStatus.innerText = "YOU WIN";
+      count++;
+      elUser.style.boxShadow = "0px 0px 0px 50px rgba(255, 255, 255, 0.05), 0px 0px 0px 100px rgba(255, 255, 255, 0.05), 0px 0px 0px 150px rgba(255, 255, 255, 0.05)";
+   
+    } else {
+      elStatus.innerText = "YOU LOSE";
+      elRobot.style.boxShadow = "0px 0px 0px 50px rgba(255, 255, 255, 0.05), 0px 0px 0px 100px rgba(255, 255, 255, 0.05), 0px 0px 0px 150px rgba(255, 255, 255, 0.05)";
+   
+      if (count >= 1) {
+        count--;
+      } else {
+        count;
+      }
+    }
+    elScore.innerText = count;
+    sessionStorage.setItem("count", count);
+  }, 1000);
 }
