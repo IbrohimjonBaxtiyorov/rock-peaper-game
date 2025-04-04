cd13683ddf3039242c69eb22c949cc8437e941f2
ibrohim0725 <baxtiyorovibrohim513@gmail.com>
2025-04-04 11:03:58 +0500

    all style end

--- js/ui-changer.js
+++ js/ui-changer.js
@@ -1,7 +1,11 @@
 import { findWinner } from "./find-winner.js";
-import { elRobot, elScore, elStatus, elUser } from "./html-elements.js";
+import { elRefresh, elRobot, elScore, elStatus, elUser } from "./html-elements.js";
 import { robotChooser } from "./robot-chooser.js";
 
+elRefresh.addEventListener('click',()=>{
+    elUser.style.boxShadow='';
+    elRobot.style.boxShadow=''
+})
 export function uiChanger(variant) {
   elUser.src = `./img/${variant}.svg`;
 
@@ -18,7 +22,6 @@ export function uiChanger(variant) {
       elStatus.innerText = "YOU WIN";
       count++;
       elUser.style.boxShadow = "0px 0px 0px 50px rgba(255, 255, 255, 0.05), 0px 0px 0px 100px rgba(255, 255, 255, 0.05), 0px 0px 0px 150px rgba(255, 255, 255, 0.05)";
-   
     } else {
       elStatus.innerText = "YOU LOSE";
       elRobot.style.boxShadow = "0px 0px 0px 50px rgba(255, 255, 255, 0.05), 0px 0px 0px 100px rgba(255, 255, 255, 0.05), 0px 0px 0px 150px rgba(255, 255, 255, 0.05)";
