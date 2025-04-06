57b293c9a2edde5e48af0e158e4634d00570644c
ibrohim0725 <baxtiyorovibrohim513@gmail.com>
2025-04-06 13:25:02 +0500

    add some styles

---
+++ plan.md
@@ -0,0 +1,10 @@
+    <!-- { -->
+      backgroundImage:{
+        triangle:"url(../img/triangle.svg)"
+      },
+      colors:{
+        "basic-bg":'#1F3757'
+      },
+      gradientColorStops:{
+        'stop':"#131537"
+    }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+ /** @type {import('tailwindcss').Config} */
+ export default {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage:{
-        triangle:"url(../img/triangle.svg)"
-      },
-      colors:{
-        "basic-bg":'#1F3757'
-      },
-      gradientColorStops:{
-        'stop':"#131537"
-    }
+        backgroundImage:{
+          triangle:"url(../img/triangle.svg)"
+        },
+        colors:{
+          "basic-bg":'#1F3757'
+        },
+        gradientColorStops:{
+          'stop':"#131537"
+      }
+  
   },
-  plugins: [require('daisyui'),],
-}
-}
+  plugins: [
+      require('daisyui'),
+  ],
+},
+ }
